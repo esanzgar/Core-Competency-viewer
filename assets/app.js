ed74de2320dece2c4afecafdb7fba5522d07268f
khawkins98 <khawkins98@gmail.com>
2017-03-27 10:49:57 +0100

    Bootstrap json

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,52 @@
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function runApp(loadedData) {
+  var ReviewCalculator = React.createClass({
+     getInitialState: function() {
+       return {data: loadedData};
+     },
+     render: function() {
+       return (
+          <table>
+            <tbody>
+             <tr>
+               <td>{this.state.data[0].name}</td>
+             </tr>
+            </tbody>
+          </table>
+       );
+
+     }
+  });
+ ReactDOM.render(<ReviewCalculator/>, document.getElementById('main-content-area'));
 }
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
 
-ReactDOM.render(
-  element,
-  document.getElementById('main-content-area')
-);
+
+
+
+
+// load the json
+$.getJSON( "/assets/datasets/ebi-cp-knowledge-base.json", function( data ) {
+  // console.log(data);
+  runApp(data);
+});
+
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName;
+// }
+//
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// };
+//
+// const element = (
+//   <h1>
+//     Hello, {formatName(user)}!
+//   </h1>
+// );
+//
+// ReactDOM.render(
+//   element,
+//   document.getElementById('main-content-area')
+// );
