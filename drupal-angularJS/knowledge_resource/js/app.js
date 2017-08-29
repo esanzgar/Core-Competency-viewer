d801778d96641acfee63fa23a3fb9f5b2d36c677
albags <gomezsegura.alba@gmail.com>
2017-08-29 17:04:36 +0100

    BioExcel front-end 1.0

--- drupal-angularJS/knowledge_resource/js/app.js
+++
@@ -1,9 +0,0 @@
-// Angular code
-(function(){
-	var compentencyProfile = angular.module('compentencyProfile', ['ngRoute', 'ngSanitize']);
-
-	compentencyProfile.config(['$compileProvider', function ($compileProvider) {
-	    $compileProvider.debugInfoEnabled(false);
-	}]);
-	
-})();
