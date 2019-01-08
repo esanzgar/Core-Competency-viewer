fce97207a149a68e7e748d2deb3589ed361d885e
Prakash Singh Gaur <prakash@ebi.ac.uk>
2019-01-08 14:19:23 +0530

    added react pages

---
+++ resource/precache-manifest.6cf9088b92b1e3ba29e6cffd14c18063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2119a707851a1d5176",
+    "url": "/resource/static/js/runtime~main.6c2119a7.js"
+  },
+  {
+    "revision": "90b54a6301291be1f611",
+    "url": "/resource/static/js/main.90b54a63.chunk.js"
+  },
+  {
+    "revision": "3ef19fa3d6fa09e52e1b",
+    "url": "/resource/static/js/1.3ef19fa3.chunk.js"
+  },
+  {
+    "revision": "90b54a6301291be1f611",
+    "url": "/resource/static/css/main.cbc4bb9b.chunk.css"
+  },
+  {
+    "revision": "f8dd2667dfc14d9289455a24befc2760",
+    "url": "/resource/index.html"
+  }
+];
